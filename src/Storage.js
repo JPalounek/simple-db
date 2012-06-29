a3f4b9a934ee70361b4ab5e466f5a1fd4397ea39
Jan Palounek <jan@palounek.com>
2012-06-30 01:00:32 +0200

    Loading implemented.

--- src/Storage.js
+++ src/Storage.js
@@ -20,12 +20,38 @@ Storage.prototype.saveStorage = function(name, data, done) {
 	});
 };
 
+Storage.prototype.recordExists = function(storage, key, cb) {
+	for (var i in storage) {
+		if(storage[i].key == key) {
+			console.log('Record found by recordExists. Its ' + i + 'th record');
+			cb(i);
+		}
+	}
+
+	console.log('Record not found by recordExists');
+	cb(false);
+}
+
 Storage.prototype.save = function(storage, data, cb) {
 	this.getStorage(storage, function (item) {
 		if(typeof item == 'undefined') {
 			item = new Array();
 			item.push(data);
 		} else {
+			/*
+			Storage.prototype.recordExists(item, data.key, function (result) {
+				console.log(typeof result);
+				if(typeof result != 'string') {
+					console.log('Not stored yet, creating new record.');
+					item.push(data);
+				} else {
+					result = parseInt(result);
+					console.log('Already stored, rewriting ' + result + 'th record from ' + item[result].val + ' to ' + data.val + '.');
+					item[result].val = data.val;
+				}
+			});
+			*/
+
 			item.push(data);
 		}
 
@@ -40,13 +66,13 @@ Storage.prototype.load = function(storage, key, cb) {
 	this.getStorage(storage, function (data) {
 		console.log('Loading ' + key);
 
-		for (var i in data) {
+		for (var i in data.reverse()) {
 			if(data[i].key == key) {
 				cb(data[i].val);
 			}
 		}
 
-		cb('Loading ' + key);
+		cb('failed');
 	});
 };
 
